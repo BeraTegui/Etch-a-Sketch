55a3abb6b0b4f0346c6a4f6ee0efc7f51f7079da
BeraTegui <manubased@gmail.com>
2025-01-13 16:57:01 -0300

    in process of styling the layout display

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function gridCreation(layout){
     for(let i = 0; i <= layout; i++){
         let square = document.createElement('div')
         square.style.backgroundColor = 'red';
-        square.style.width = '50px';
-        square.style.height = '10px';
-        square.style.border = 'solid, white';
+        square.style.width = '75px';
+        square.style.height = '75px';
+        square.style.padding = '0px';
+        
 
         container.appendChild(square);
     }
