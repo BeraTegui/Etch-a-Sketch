15862f5fb5ae3d93632c0a78f17b152e8fd7d335
BeraTegui_ <manubased@gmail.com>
2025-01-13 21:34:12 -0300

    Update script.js manually

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector('.container');
 
 function gridCreation(layout){
-    for(let i = 0; i <= layout; i++){
-        let square = document.createElement('div')
-        square.style.backgroundColor = 'red';
-        square.style.width = '75px';
-        square.style.height = '75px';
-        square.style.padding = '0px';
-        
-
-        container.appendChild(square);
+    for(let i = 1; i <= layout; i++){
+        const auxRow = document.createElement('div');
+        auxRow.style.display = 'flex';
+        for(let j = 1; j <= layout; j++){
+            let block = document.createElement('div')
+            block.style.backgroundColor = 'white';
+            block.style.border = 'solid 5px black'
+            block.style.width = '45px';
+            block.style.height = '45px';
+            block.style.padding = '0px';
+            block.style.flexShrink = 1;
+            
+            auxRow.appendChild(block);
+        }
+        container.appendChild(auxRow);
     }
 }
