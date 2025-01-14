71a3be67af8f13876be3da453a025b6cc181ac98
BeraTegui <manubased@gmail.com>
2025-01-14 09:42:49 -0300

    change block color on hover (not revert)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+let color = '';
 
 function gridCreation(layout){
     for(let i = 1; i <= layout; i++){
@@ -6,17 +7,43 @@ function gridCreation(layout){
         auxRow.style.display = 'flex';
         for(let j = 1; j <= layout; j++){
             let block = document.createElement('div')
-            block.style.backgroundColor = 'white';
+            block.classList = "block";
+            block.style.backgroundColor = 'rgb(197, 202, 233)';
             block.style.border = 'solid 5px black'
             block.style.width = '45px';
             block.style.height = '45px';
             block.style.padding = '0px';
             block.style.flexShrink = 1;
+            let color = block.style.backgroundColor;
             
             auxRow.appendChild(block);
+            
         }
         container.appendChild(auxRow);
     }
+    
 }
 
+
+
 gridCreation(16);
+
+const block = document.querySelectorAll('.block');
+console.log(block);
+
+function changeColor(element){
+    element.style.backgroundColor = 'white';
+}
+
+function colorBack(element){
+    element.style.backgroundColor = color;
+}
+
+block.forEach((currentValue) =>{
+    currentValue.addEventListener('mouseover', () =>{
+        changeColor(currentValue);
+    });
+    /* currentValue.addEventListener('mouseout', () =>{
+        colorBack(currentValue);
+    }); */
+});
